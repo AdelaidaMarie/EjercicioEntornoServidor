82cbfea27e56ec3f27e9adc37e3bf16a7bfe6843
AdelaidaMarie <adavegla@gmail.com>
2021-10-29 04:42:09 +0200

    Carpeta de repositorio remoto

---
+++ Adios.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php echo "Hasta luego desarrollador!!!"; ?>
+</body>
+</html>
---
+++ Hola.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Mi primera página PHP</h1>
+<?php echo "Hola mundo!"; ?>
+</body>
+</html>
+Salida:
+Hola mundo!
